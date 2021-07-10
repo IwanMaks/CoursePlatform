bed58c5b53ade2356be94113f05a56ff7b8b8103
Iwan <iwan.maksackov@yandex.ru>
2021-07-10 13:24:12 +0300

    reform style for input component

--- components/Input/Input.tsx
+++ components/Input/Input.tsx
@@ -1,12 +1,13 @@
 import {InputProps} from "./Input.props";
 import styles from './Input.module.css';
+import globalTextStyle from '../../styles/Home.module.css'
 import cn from 'classnames'
 
 export const Input = ({className, ...props}: InputProps): JSX.Element => {
     return (
         <div className={cn(className, styles.inputWrapper)}>
             <input
-                className={styles.input}
+                className={cn(styles.input, globalTextStyle.body_normal_regular)}
                 {...props}
             />
         </div>
